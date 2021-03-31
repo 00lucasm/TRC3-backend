c2e059b2ed3fc89ae00806fa8ace7704d1a5cfe0
Melo-Lucas <lucs.r06@gmail.com>
2021-03-31 19:57:19 -0300

    Adicionado: READ (GET method) e UPDATE (POST method) à aplicação

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -19,6 +19,36 @@ class UserController{
 			return res.status(500).json({message: `Erro interno: ${error}`})
 		}
 	}
+
+	//READ with GET method
+	async index(req, res){
+		try{
+			const users = await User.find()
+
+			return res.status(200).json(users);
+		}catch(error){
+			return res.status(500).json({message: `Erro interno: ${error}`})
+		}
+	}
+
+	//UPDATE with PUT/PATCH method
+	async update(req, res){
+		try{
+			const { id } = req.params;
+			const user = await User.findOne({ id })
+			if(!user){
+				return res.status(400).json({message: "user not found!"})
+			}
+
+			const { name, email } = req.body
+			const user_updated = { name, email }
+			await User.update(user_updated)
+			return res.status(201).json(user_updated)
+		}catch(error){
+			return res.status(500).json({message: `Erro interno: ${error}`})
+		}
+		
+	}
 }
  
 export default new UserController();
