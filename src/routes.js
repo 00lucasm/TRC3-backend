3c9caa9f33fc781185a16089a80fb31f1e665f98
Melo-Lucas <lucs.r06@gmail.com>
2021-03-31 19:58:04 -0300

    Adicionado rota para requisição GET e requisição POST

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import UserController from './app/controllers/UserController'
 
 const routes = new Router();
 
-routes.post("/users", UserController.store)
-
+routes.post("/users", UserController.store);
+routes.get("/users", UserController.index);
+routes.put("/users/:id", UserController.update);
 export default routes;
